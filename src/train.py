73a602963a01002dd370723ef11c0f52512d864f
ekdnam <adeetya.m@gmail.com>
2020-10-02 00:24:44 +0530

    add model reference in train.py

--- src/train.py
+++ src/train.py
@@ -10,7 +10,7 @@ DEVICE = "cpu"
 EPOCHS = 1000
 
 
-def run_training(fold):
+def run_training(fold, save_model=False):
     df = pd.read_csv("../input/train_features.csv")
     df = df.drop(["cp_time", "cp_dose", "cp_type"], axis=1)
 
@@ -41,6 +41,37 @@ def run_training(fold):
     valid_loader = torch.utils.data.DataLoader(
         valid_dataset, batch_size=1024, num_workers=8
     )
+
+    model = utils.Model(
+        nfeatures=xtrain.shape[1],
+        ntargets=ytrain.shape[1],
+        nlayers=2,
+        hidden_size=128,
+        dropout=0.3,
+    )
+
+    model.to(DEVICE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    eng = utils.Engine(model, optimizer, device=DEVICE)
+
+    best_loss = np.inf
+    early_stopping_iter = 10
+    early_stopping_counter = 0
+
+    for epoch in EPOCHS:
+        train_loss = eng.train(train_loader)
+        valid_loss = eng.evaluate(valid_loader)
+        print(f"{fold}, {epoch}, {train_loss}, {valid_loss}")
+        if valid_loss < best_loss:
+            best_loss = valid_loss
+            if save_model:
+                torch.save(model.state_dict(), f"model_{fold}.bin")
+        else:
+            early_stopping_counter += 1
+
+        if early_stopping_counter > early_stopping_iter:
+            break
 
 
 if __name__ == "__main__":
