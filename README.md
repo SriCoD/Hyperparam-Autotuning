adc9fd20ec59bfb902c4efc49494650cdb38e4b9
ekdnam <adeetya.m@gmail.com>
2020-10-01 22:10:03 +0530

    add readme.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Hyperparam-Autotuning
  Using optuna
+
+## Get data
+
+In the cmd, type
+```
+git clone https://github.com/ekdnam/hyperparam-autotuning
+```
+
+Then, type
+```
+mkdir input
+```
+
+After that
+```
+kaggle competitions download -c lish-moa
+```
+
+The dataset would be downloaded.
+
---
+++ src/train.py
@@ -0,0 +1,20 @@
+import torch
+import pandas as pd
+import numpy as np
+
+DEVICE = "cuda"
+EPOCHS = 1000
+
+def run_training(fold):
+    df = pd.read_csv("../inputs/train_features.csv")
+    df = df.drop(["cp_time", "cp_dose", "cp_type"], axis = 1)
+
+    targets_df = pd.read_csv("../input/train_targets_folds.csv")
+
+    feature_columns = df.drop("sig_id", axis = 1).columns
+    targets_columns = targets_df.drop("sig_id", axis = 1).columns
+
+    df = df.merge(targets_df, on = "sig_id", how = "left")
+
+if __name__ == '__main__':
+    run_training(fold=0)
