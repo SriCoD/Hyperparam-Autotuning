7387cd399185fa0d4d5cc05c1ef384147d23b82b
ekdnam <adeetya.m@gmail.com>
2020-10-02 00:03:54 +0530

    add datalaoder

--- src/train.py
+++ src/train.py
@@ -2,20 +2,46 @@ import torch
 import pandas as pd
 import numpy as np
 
+import utils
+
+
 DEVICE = "cuda"
+DEVICE = "cpu"
 EPOCHS = 1000
+
 
 def run_training(fold):
     df = pd.read_csv("../input/train_features.csv")
-    df = df.drop(["cp_time", "cp_dose", "cp_type"], axis = 1)
+    df = df.drop(["cp_time", "cp_dose", "cp_type"], axis=1)
 
     targets_df = pd.read_csv("../input/train_targets_folds.csv")
 
-    feature_columns = df.drop("sig_id", axis = 1).columns
-    targets_columns = targets_df.drop("sig_id", axis = 1).columns
+    feature_columns = df.drop("sig_id", axis=1).columns
+    target_columns = targets_df.drop("sig_id", axis=1).columns
 
-    df = df.merge(targets_df, on = "sig_id", how = "left")
-    print(df)
+    df = df.merge(targets_df, on="sig_id", how="left")
+    # print(df)
 
-if __name__ == '__main__':
+    train_df = df[df.kfold != fold].reset_index(drop=True)
+    valid_df = df[df.kfold == fold].reset_index(drop=True)
+
+    xtrain = train_df[feature_columns].to_numpy()
+    ytrain = train_df[target_columns].to_numpy()
+
+    xvalid = valid_df[feature_columns].to_numpy()
+    yvalid = valid_df[target_columns].to_numpy()
+
+    train_dataset = utils.MOADataset(features=xtrain, targets=ytrain)
+    valid_dataset = utils.MOADataset(features=xvalid, targets=yvalid)
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=1024, num_workers=8, shuffle=True
+    )
+
+    valid_loader = torch.utils.data.DataLoader(
+        valid_dataset, batch_size=1024, num_workers=8
+    )
+
+
+if __name__ == "__main__":
     run_training(fold=0)
--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,43 @@
 import torch
+import torch.nn as nn
+
 
 class MOADataset:
     def __init__(self, features, targets):
         # both features and targets are numpy arrays
         self.features = features
         self.targets = targets
-    
+
     def __getitem__(self):
         return {
-            "x": torch.tensor(self.features[item, :], dtype = torch.float),
-            "y": torch.tensor(self.targets[item, :], dtype = torch.float)
+            "x": torch.tensor(self.features[item, :], dtype=torch.float),
+            "y": torch.tensor(self.targets[item, :], dtype=torch.float),
         }
+
     def __len__(self):
         return self.features.shape[0]
+
+
+class Engine:
+    def __init__(self, model, optimizer, device):
+        self.model = model
+        self.device = device
+        self.optimizer = optimizer
+
+    @staticmethod
+    def loss_fn(targets, outputs):
+        return nn.BCEWithLogitsLoss()(outputs, targets)
+
+    def train(self, data_loader):
+        self.model.train()
+        final_loss = 0
+        for data in data_loader:
+            self.optimizer.zero_grad()
+            inputs = data["x"].to(self.device)
+            targets = data["y"].to(self.device)
+            outputs = self.model(inputs)
+            loss = self.loss_fn(targets, outputs)
+            loss.backward()
+            self.optimizer.step()
+            final_loss += loss.item()
+        return final_loss / len(data_loader)
