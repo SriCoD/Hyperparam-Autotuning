a220d5f0aaf74aa56a930df888a6b53748788329
ekdnam <adeetya.m@gmail.com>
2020-10-01 23:48:43 +0530

    error in filepath

--- src/train.py
+++ src/train.py
@@ -6,7 +6,7 @@ DEVICE = "cuda"
 EPOCHS = 1000
 
 def run_training(fold):
-    df = pd.read_csv("../inputs/train_features.csv")
+    df = pd.read_csv("../input/train_features.csv")
     df = df.drop(["cp_time", "cp_dose", "cp_type"], axis = 1)
 
     targets_df = pd.read_csv("../input/train_targets_folds.csv")
@@ -15,6 +15,7 @@ def run_training(fold):
     targets_columns = targets_df.drop("sig_id", axis = 1).columns
 
     df = df.merge(targets_df, on = "sig_id", how = "left")
+    print(df)
 
 if __name__ == '__main__':
     run_training(fold=0)
